7e65a34c97095f37aed40e809c8f969d8cbc696a
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-21 10:28:31 +0100

    add two factor authentication plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/database";
+import { twoFactor } from "better-auth/plugins";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  appName: "Next.js Marketplace Experiment",
   database: prismaAdapter(prisma, {
     provider: "sqlite",
   }),
   emailAndPassword: {
     enabled: true,
-  }
+  },
+  plugins: [twoFactor()],
 });
 
 export const { POST, GET } = toNextJsHandler(auth);
