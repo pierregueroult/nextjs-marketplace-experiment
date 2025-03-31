c359bd9f8f1d8c0af5e8fc464bdbaf0051c7ed5c
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-31 16:02:32 +0200

    integrate environment variable management with @t3-oss/env-nextjs

--- src/emails/send.ts
+++ src/emails/send.ts
@@ -2,6 +2,7 @@ import type { ReactElement } from "react";
 import type { MailOptions } from "nodemailer/lib/sendmail-transport";
 import { render } from "@react-email/render";
 import transporter from "@/emails/transporter";
+import { env } from "@/lib/env";
 
 type EmailSendOptions = Omit<MailOptions, "html"> & {
   template: ReactElement<unknown> | string;
@@ -12,7 +13,7 @@ export default async function send({ template, ...options }: EmailSendOptions): 
 
   try {
     await transporter.sendMail({
-      from: process.env.EMAIL_FROM,
+      from: env.SMTP_EMAIL_FROM,
       html,
       ...options,
     });
