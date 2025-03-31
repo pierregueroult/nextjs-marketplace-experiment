b43f3d5e43d3ccb6e1d8ece3a964fef1ee756f91
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-31 14:05:42 +0200

    feat(emails): add email sending functionality with nodemailer (#3)
    * add email sending functionality with nodemailer
    * Rename type for email sending options and add secure transport configuration

---
+++ src/emails/transporter.ts
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT ?? "25", 10),
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  secure: process.env.SMTP_SECURE === "true",
+});
+
+export default transporter;
