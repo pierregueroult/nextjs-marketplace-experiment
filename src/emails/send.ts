5d960ff7158fe9b6b7dcc3208aa71413892eeba3
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-04-01 12:41:12 +0200

    refactor email transport setup and update environment variables

--- src/emails/send.ts
+++ src/emails/send.ts
@@ -1,8 +1,10 @@
 import type { ReactElement } from "react";
 import type { MailOptions } from "nodemailer/lib/sendmail-transport";
 import { render } from "@react-email/render";
-import transporter from "@/emails/transporter";
 import { env } from "@/lib/env";
+import { createTransport } from "nodemailer";
+
+const transporter = createTransport(env.EMAIL_SERVER);
 
 type EmailSendOptions = Omit<MailOptions, "html"> & {
   template: ReactElement<unknown> | string;
@@ -11,9 +13,11 @@ type EmailSendOptions = Omit<MailOptions, "html"> & {
 export default async function send({ template, ...options }: EmailSendOptions): Promise<boolean> {
   const html = typeof template === "string" ? template : await render(template);
 
+  console.log('Now sending email to', options.to);
+
   try {
     await transporter.sendMail({
-      from: env.SMTP_EMAIL_FROM,
+      from: env.EMAIL_FROM,
       html,
       ...options,
     });
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,15 +4,8 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    SMTP_EMAIL_FROM: z.string().email(),
-    SMTP_SERVER_HOST: z.string(),
-    SMTP_SERVER_PORT: z
-      .string()
-      .regex(/^\d+$/)
-      .transform((val) => parseInt(val, 10)),
-    SMTP_SERVER_USER: z.string(),
-    SMTP_SERVER_PASS: z.string(),
-    SMTP_SERVER_SECURE: z.string().default("false").transform(val => val === "true"),
+    EMAIL_FROM: z.string().email(),
+    EMAIL_SERVER: z.string().url(),
     BETTER_AUTH_SECRET: z.string(),
     BETTER_AUTH_URL: z.string().url(),
   },
@@ -21,12 +14,8 @@ export const env = createEnv({
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    SMTP_EMAIL_FROM: process.env.SMTP_EMAIL_FROM,
-    SMTP_SERVER_HOST: process.env.SMTP_SERVER_HOST,
-    SMTP_SERVER_PORT: process.env.SMTP_SERVER_PORT,
-    SMTP_SERVER_USER: process.env.SMTP_SERVER_USER,
-    SMTP_SERVER_PASS: process.env.SMTP_SERVER_PASS,
-    SMTP_SERVER_SECURE: process.env.SMTP_SERVER_SECURE,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    EMAIL_SERVER: process.env.EMAIL_SERVER,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
