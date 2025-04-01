c9a46959f9c9146b96be10f9351ea43ae97fbfa7
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-04-01 14:48:50 +0200

    clean document structure and add a proper header with links and categories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { ReactNode } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 type RootLayoutProps = Readonly<{
   children: ReactNode;
@@ -11,13 +12,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" suppressHydrationWarning={true}>
       <head />
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem={true}
-          disableTransitionOnChange={true}
-        >
-          {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true} disableTransitionOnChange={true}>
+          <div className="flex min-h-screen flex-col">
+            <Header />
+            {children}
+          </div>
         </ThemeProvider>
       </body>
     </html>
